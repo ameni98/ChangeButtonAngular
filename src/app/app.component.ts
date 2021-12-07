76cedbee1a611161ce0cb25f0dbd1f4554bd0106
Gouja Ameni <gouja.ameni@gmail.com>
2021-12-07 12:19:17 +0100

    add all afiles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import{Glyphicons}from './glyphicons';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  glyphicons=Glyphicons;
   title = 'exercice7';
+  styles=[
+    "default","primary",'success',"info","warning","danger","link"
+  ];
+  valeur="Okay";
+  style;
+  Gly;
+  etat;
+  selectStyle(ev)
+  {
+    this.style="btn btn-"+ev['style'];
+  }
+  selectGly(val)
+  {this.Gly="bi-"+val["Gly"];
+    console.log(val['Gly']);
+  }
+  eventCheck(ev)
+  {
+    console.log(ev.name);
+    this.etat=ev.name;
+   /* if(ev.name=="active"){this.etat=false;}
+    else if(ev.name=="disabled"){this.etat=true;}*/
+    
+  }
 }
---
+++ src/app/glyphicons.spec.ts
@@ -0,0 +1,7 @@
+import { Glyphicons } from './glyphicons';
+
+describe('Glyphicons', () => {
+  it('should create an instance', () => {
+    expect(new Glyphicons()).toBeTruthy();
+  });
+});
